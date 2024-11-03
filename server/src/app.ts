241ab49b794a5fadce004d5f0dd0c230e0d310d2
Dharma001 <demonplays466@gmail.com>
2024-11-03 22:10:58 +0545

    add: added schema of a user: learned about virtual attributes(casting)

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,10 @@ import express from 'express';
 const app = express();
 const port = 5000;
 
+app.get("/products", (req,res) => {
+  res.send("hello")
+})
+
 app.listen(port , () => {
   console.log(`Server is working`)
 })
